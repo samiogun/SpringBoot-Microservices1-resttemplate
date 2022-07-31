5def1865db3a4ffffad8342c696f1151ddb78e4b
OGÜN <samiogunersun@gmail.com>
2022-07-31 21:03:18 +0300

    CustomerController.java sınıfında registerCustomer methoduna @PostMapping anotasyonu eklendi.

--- customer-service/src/main/java/org/ersun/customer/CustomerController.java
+++ customer-service/src/main/java/org/ersun/customer/CustomerController.java
@@ -3,6 +3,7 @@ package org.ersun.customer;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ public class CustomerController {
     @Autowired
     private CustomerService customerService;
 
+    @PostMapping
     public void registerCustomer(@RequestBody CustomerRegistrationRequest customerRegistrationRequest){
         log.info("new customer registration {}", customerRegistrationRequest);
         customerService.registerCustomer(customerRegistrationRequest);

a7c2abe05e9a48e5945758281191581061620aa0
OGÜN <samiogunersun@gmail.com>
2022-08-04 22:48:42 +0300

    Eurake server eklendi. Servisler Eureka client olarak kodlandı.

--- customer-service/src/main/java/org/ersun/CustomerServiceApplication.java
+++ customer-service/src/main/java/org/ersun/CustomerServiceApplication.java
@@ -2,9 +2,11 @@ package org.ersun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CustomerServiceApplication {
 
     public static void main(String[] args) {
